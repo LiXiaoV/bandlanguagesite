d79f08acb6d915f087926815d3250ff080de8e5a
lixiaov <1011529903@qq.com>
2021-06-17 17:54:32 +0800

    解决合并时有误的地方

--- bandlanguagesite-java/src/test/java/com/bandlanguage/bandlanguagesite/BandlanguagesiteApplicationTests.java
+++ bandlanguagesite-java/src/test/java/com/bandlanguage/bandlanguagesite/BandlanguagesiteApplicationTests.java
@@ -166,10 +166,4 @@ class BandlanguagesiteApplicationTests {
 
     }
 
-    @Test
-    public void testGetSceneByKeyword(){
-        List<Scene> scenes=sceneMapper.getScenesByKeyword("部门");
-        System.out.println(scenes);
-    }
-
 }
